9d42eb3e9ae15b2f9cf0686fe7f7dd2416cfcc50
Hieronymus Crouse <hcrouse1066@gmail.com>
2018-03-01 11:28:40 +0200

    <Added a bunch of unit tests and doccumentation>

--- luxon/utils/password.py
+++ luxon/utils/password.py
@@ -137,16 +137,9 @@ def valid(password, hashed):
                     "plaintext" ]
         pwd_context = passlib.context.CryptContext(schemes=schemes)
 
-    # Validate Password using pwd_context
-    # with Timer() as elapsed:
-    #     val = pwd_context.verify(password, hashed)
-    #     log.debug('Hash Validated %s' % val +
-    #               ' (DURATION: %.4fs)' % elapsed())
-    #     return val
+    #Validate Password using pwd_context
+    with Timer() as elapsed:
+        val = pwd_context.verify(password, hashed)
+        log.debug('Hash Validated %s' % val, timer=elapsed())
 
-    #(Rony)This raises the error:  TypeError: a float is required
-    #Sorry if commenting this out breaks anything :(
-
-    val = pwd_context.verify(password, hashed)
-    return val
-
+        return val
